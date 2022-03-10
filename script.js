32fe15ac534455a2d5a23ab5570ad3c458fe4645
Shahid Ameen <43674745+iamshahidameen@users.noreply.github.com>
2022-03-11 02:54:16 +0500

    Added the Transition Cubic-bezier give it a nice effect, and with the help of js we have first converted  all the characters in the label into an array then wrap  that single character in  a span with the styling of transition delay according to it's index number.

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-alert("Form, Wave!");
+const fieldLabel = document.querySelectorAll(".fieldLabel");
+
+fieldLabel.forEach((label) => {
+    label.innerHTML = label.innerText
+    .split('')
+    .map((letter, i) => `<span style="transition-delay: ${i * 100}ms">${letter}</span>`).join('');
+    
+    console.log(label)
+    
+})
